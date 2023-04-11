c63133c36a06b15182deb121ec680734a155e928
Adhulka <Adhulkanand@gmail.com>
2023-04-11 10:03:49 +0530

    commit

---
+++ bin/list.dart
@@ -0,0 +1,44 @@
+void main() {
+  // ///list.filled
+  // var c = List.filled(10, 1, growable: true);
+  // c.add(7);
+  // c[3] = 8;
+  // c[10] = 1;
+  // print(c);
+
+  // ///list.from
+  // var d = List.from(c);
+  // d.addAll(c);
+  // print("d=$d");
+
+  // ///list.off
+  // var f = List.of(c);
+  // f.addAll(c);
+  // print("f=$f");
+
+  // ///list.unmodifiable
+  // List<int> h = List.unmodifiable([1, 2, 3, 4, 5, 6]);
+  // //f,addAll(["hello,hai,good"]);shows errors since there is no property named growable
+
+  // print("h=$h");
+
+  //list.generate
+  // var x = List.generate(10, (a) => a * 4);
+  // print("x=$x");
+
+  // int reminder, sum = 0, temp;
+  // int number = 56565;
+  // temp = number;
+  // // a loop to reverse a number
+  // while (number > 0) {
+  //   reminder = number % 10; //get remainder
+  //   sum = (sum * 10) + reminder;
+  //   number = number ~/ 10;
+  // }
+
+  // if (sum == temp) {
+  //   print('Its A Palindrome number');
+  // } else {
+  //   print('Its A Not Palindrome number');
+  // }
+}
