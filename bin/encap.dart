9cbd62da7c43ff816e058bba01c96ed44068cf20
Adhulka <Adhulkanand@gmail.com>
2023-04-24 11:35:25 +0530

    commit

---
+++ bin/encap.dart
@@ -0,0 +1,10 @@
+import 'encapDemo.dart';
+
+void main() {
+  deatils obj = deatils();
+
+  obj.emp1 = "anu";
+  int pin = obj.compin;
+  print(obj.staffname);
+  print(pin);
+}
---
+++ bin/encapDemo.dart
@@ -0,0 +1,48 @@
+class deatils {
+  String? name;
+  int? age;
+  String? job;
+  int pin = 6800;
+  String company = "abc";
+
+  set emp1(String emp1) {}
+  set empname(String name) {
+    this.name = name;
+  }
+
+  set empage(int age) {
+    this.age = age;
+  }
+
+  set empjob(String job) {
+    this.job = job;
+  }
+
+  set empcomp(String company) {
+    this.company = company;
+  }
+
+  set compin(int pin) {
+    this.pin = pin;
+  }
+
+  String? get staffname {
+    return name;
+  }
+
+  int? get staffage {
+    return age;
+  }
+
+  String? get staffjob {
+    return job;
+  }
+
+  String get staffcompany {
+    return company;
+  }
+
+  int get compin {
+    return pin;
+  }
+}
